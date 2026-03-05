6232db695eb8065b35f30813afa194e5652a1b2b
Zephaniah Flynn <18018780+ZDevelop94@users.noreply.github.com>
2026-03-05 12:05:57 +0000

    [CARF-377] Implements Create Enrolment API call stubs
    # Conflicts:
    #	conf/app.routes

---
+++ app/uk/gov/hmrc/carfstubs/models/request/Enrolment.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.carfstubs.models.request
+
+import play.api.libs.json.*
+
+case class Enrolment(identifiers: Seq[Identifier], verifiers: Seq[Verifier])
+
+case class Identifier(key: String, value: String) extends KeyValues
+case class Verifier(key: String, value: String) extends KeyValues
+
+trait KeyValues {
+  val key: String
+  val value: String
+}
+
+object Enrolment {
+  implicit val formatEnrollment: OFormat[Enrolment]  = Json.format[Enrolment]
+  implicit val formatIdentifier: OFormat[Identifier] = Json.format[Identifier]
+  implicit val formatVerifier: OFormat[Verifier]     = Json.format[Verifier]
+}
---
+++ test/controllers/EnrolmentControllerSpec.scala
@@ -0,0 +1,189 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import org.scalatest.matchers.must.Matchers
+import play.api.http.Status.NO_CONTENT
+import play.api.libs.json.{JsValue, Json}
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import uk.gov.hmrc.carfstubs.controllers.routes
+
+class EnrolmentControllerSpec extends SpecBase with Matchers {
+
+  "EnrolmentController" - {
+    "upsertEnrolment" - {
+      s"must return NoContent - $NO_CONTENT for a valid json" in {
+        val json: JsValue = Json.parse(
+          s"""
+             |{
+             |  "identifiers": [ { "key": "UTR", "value": "AA000003D" } ],
+             |  "verifiers": [
+             |          {
+             |              "key": "PostCode",
+             |              "value": "N15 2FY"
+             |          },
+             |          {
+             |              "key": "IsAbroad",
+             |              "value": "N"
+             |          }
+             |   ]
+             |}
+             |""".stripMargin
+        )
+
+        val request = FakeRequest(PUT, routes.EnrolmentController.upsertEnrolment.url).withBody(json)
+        val result  = route(app, request).value
+
+        status(result) mustBe NO_CONTENT
+      }
+
+      s"must return Bad Request - $BAD_REQUEST for a UTR value starts with (8)" in {
+        val json: JsValue = Json.parse(
+          s"""
+             |{
+             |  "identifiers": [ { "key": "UTR", "value": "8A000008D" } ],
+             |  "verifiers": [
+             |          {
+             |              "key": "PostCode",
+             |              "value": "N15 2FY"
+             |          },
+             |          {
+             |              "key": "IsAbroad",
+             |              "value": "N"
+             |          }
+             |   ]
+             |}
+             |""".stripMargin
+        )
+
+        val request = FakeRequest(PUT, routes.EnrolmentController.upsertEnrolment.url).withBody(json)
+        val result  = route(app, request).value
+
+        status(result)        mustBe BAD_REQUEST
+        contentAsString(result) must include(
+          "Provided service name is not in services-to-activate or No group ID in active auth session"
+        )
+      }
+      s"must return Bad Request - $BAD_REQUEST for a UTR value starts with (X)" in {
+        val json: JsValue = Json.parse(
+          s"""
+             |{
+             |  "identifiers": [ { "key": "UTR", "value": "XA000008D" } ],
+             |  "verifiers": [
+             |          {
+             |              "key": "PostCode",
+             |              "value": "N15 2FY"
+             |          },
+             |          {
+             |              "key": "IsAbroad",
+             |              "value": "N"
+             |          }
+             |   ]
+             |}
+             |""".stripMargin
+        )
+
+        val request = FakeRequest(PUT, routes.EnrolmentController.upsertEnrolment.url).withBody(json)
+        val result  = route(app, request).value
+
+        status(result)        mustBe BAD_REQUEST
+        contentAsString(result) must include(
+          "Provided service name is not in services-to-activate or No group ID in active auth session"
+        )
+      }
+      s"must return Bad Request - $BAD_REQUEST for a request with empty identifiers" in {
+        val json: JsValue = Json.parse(
+          s"""
+             |{
+             |  "identifiers": [],
+             |  "verifiers": [
+             |          {
+             |              "key": "PostCode",
+             |              "value": "N15 2FY"
+             |          },
+             |          {
+             |              "key": "IsAbroad",
+             |              "value": "N"
+             |          }
+             |   ]
+             |}
+             |""".stripMargin
+        )
+
+        val request = FakeRequest(PUT, routes.EnrolmentController.upsertEnrolment.url).withBody(json)
+        val result  = route(app, request).value
+
+        status(result)        mustBe BAD_REQUEST
+        contentAsString(result) must include(
+          "Provided service name is not in services-to-activate or No group ID in active auth session"
+        )
+      }
+      s"must return Bad Request - $INTERNAL_SERVER_ERROR for a UTR value starts with (9)" in {
+        val json: JsValue = Json.parse(
+          s"""
+             |{
+             |  "identifiers": [ { "key": "UTR", "value": "9A000008D" } ],
+             |  "verifiers": [
+             |          {
+             |              "key": "PostCode",
+             |              "value": "N15 2FY"
+             |          },
+             |          {
+             |              "key": "IsAbroad",
+             |              "value": "N"
+             |          }
+             |   ]
+             |}
+             |""".stripMargin
+        )
+
+        val request = FakeRequest(PUT, routes.EnrolmentController.upsertEnrolment.url).withBody(json)
+        val result  = route(app, request).value
+
+        status(result)        mustBe INTERNAL_SERVER_ERROR
+        contentAsString(result) must include("Internal Server Error")
+      }
+      s"must return Bad Request - $INTERNAL_SERVER_ERROR for a UTR value starts with (Y)" in {
+        val json: JsValue = Json.parse(
+          s"""
+             |{
+             |  "identifiers": [ { "key": "UTR", "value": "YA000008D" } ],
+             |  "verifiers": [
+             |          {
+             |              "key": "PostCode",
+             |              "value": "N15 2FY"
+             |          },
+             |          {
+             |              "key": "IsAbroad",
+             |              "value": "N"
+             |          }
+             |   ]
+             |}
+             |""".stripMargin
+        )
+
+        val request = FakeRequest(PUT, routes.EnrolmentController.upsertEnrolment.url).withBody(json)
+        val result  = route(app, request).value
+
+        status(result)        mustBe INTERNAL_SERVER_ERROR
+        contentAsString(result) must include("Internal Server Error")
+      }
+    }
+  }
+}
